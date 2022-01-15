00d4ce52ad8665386b3d15ac7e9badfe71587a8d
vladorjiggy <vladislavfetisov@gmail.com>
2022-01-15 18:27:44 +0100

    categorylinks

--- frontend/src/components/CategoryRenderList.js
+++ frontend/src/components/CategoryRenderList.js
@@ -1,10 +1,11 @@
+import { Link } from "react-router-dom"
 const CategoryRenderList = props => {
     console.log(props)
     const rows = props.categories.map((row, index) => {
         return (
 
             <li id={"link-li" + index+1}>
-                <a data-id={row._id}>{row.name}</a>
+                <Link to={"/post/category/" + row._id}>{row.name}</Link>
             </li>
         )
     })
