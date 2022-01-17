316f6d40245d1cb373ad6eec52306691394cadcb
s78277 <s78277@beuth-hochschule.de>
2022-01-17 18:13:35 +0100

    searchimg in public ordner

--- frontend/src/pages/UeberUns.js
+++ frontend/src/pages/UeberUns.js
@@ -8,7 +8,7 @@ class UeberUns extends Component {
         <article id="article1-section1-aboutUs">
           <img
             id="article1-img-aboutUs"
-            src="../layout/images/aboutUs.jpeg"
+            src="searchImages/aboutUs.jpeg"
             alt="ueberUns_img"
           />
         </article>
